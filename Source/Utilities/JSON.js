4f98ae9146459a032765884d8f7b48518e552fa2
Valerio Proietti <kamicane@gmail.com>
2011-06-22 05:14:44 +0200

    - updated JSON

--- Source/Utilities/JSON.js
+++ Source/Utilities/JSON.js
@@ -2,14 +2,12 @@
 ---
 name: JSON
 description: JSON encoder and decoder
-requires: [typeOf, Array, String]
-provides: JSON
 ...
 */
 
-if (!this.JSON) this.JSON = {};
-
-(function(){
+define('JSON', ['Utility/typeOf', 'Host/Array'], function(typeOf, Array){
+	
+var json = function(){}, JSON = (typeof JSON == 'undefined') ? {} : JSON;
 
 var special = {'\b': '\\b', '\t': '\\t', '\n': '\\n', '\f': '\\f', '\r': '\\r', '"' : '\\"', '\\': '\\\\'};
 
@@ -17,45 +15,49 @@ var escape = function(chr){
 	return special[chr] || '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
 };
 
-JSON.validate = function(string){
+json.validate = function(string){
 	string = string.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@').
 					replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
 					replace(/(?:^|:|,)(?:\s*\[)+/g, '');
-	
+
 	return (/^[\],:{}\s]*$/).test(string);
 };
 
-JSON.encode = JSON.stringify || function(obj){
+json.encode = JSON.stringify || function(obj){
 	if (obj && obj.toJSON) obj = obj.toJSON();
-	
+
 	switch (typeOf(obj)){
 		case 'string':
 			return '"' + obj.replace(/[\x00-\x1f\\"]/g, escape) + '"';
 		case 'array':
-			return '[' + obj.map(JSON.encode).clean() + ']';
+			return '[' + Array.filter(Array.map(obj, json.encode), function(item){
+				return item != null;
+			}) + ']';
 		case 'object':
 			var string = [];
 			for (var key in obj){
-				var json = JSON.encode(obj[key]);
-				if (json) string.push(JSON.encode(key) + ':' + json);
+				var json = json.encode(obj[key]);
+				if (json) string.push(json.encode(key) + ':' + json);
 			}
 			return '{' + string + '}';
 		case 'number': case 'boolean': return '' + obj;
 		case 'null': return 'null';
 	}
-	
+
 	return null;
 };
 
-JSON.decode = function(string, secure){
+json.decode = function(string, secure){
 	if (!string || typeOf(string) != 'string') return null;
-	
-	if (secure || JSON.secure){
+
+	if (secure || json.secure){
 		if (JSON.parse) return JSON.parse(string);
-		if (!JSON.validate(string)) throw new Error('JSON could not decode the input; security is enabled and the value is not secure.');
+		if (!json.validate(string)) throw new Error('JSON could not decode the input; security is enabled and the value is not secure.');
 	}
-	
+
 	return eval('(' + string + ')');
 };
 
-})();
+return json;
+
+});
