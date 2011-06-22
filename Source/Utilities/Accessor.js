5f356fd730d10a3e4ef30c21335dae1ba385bc35
Valerio Proietti <kamicane@gmail.com>
2011-06-22 05:10:59 +0200

    - updated accessor

--- Source/Utilities/Accessor.js
+++ Source/Utilities/Accessor.js
@@ -1,53 +1,65 @@
 /*
 ---
 name: Accessor
-description: Accessor
-requires: [typeOf, Array, Function, String, Object]
-provides: Accessor
+description: Access things
 ...
 */
 
-(function(){
+define('Utility/Accessor', ['Utility/typeOf', 'Utility/forEach', 'Host/Array'], function(typeOf, forEach, Array){
 
-this.Accessor = function(singular, plural){
-	
+return function(singular, plural){
+
 	var accessor = {}, matchers = [];
-	
-	singular = (singular || '').capitalize();
+
 	if (!plural) plural = singular + 's';
-	
+
 	var define = 'define', lookup = 'lookup', match = 'match', each = 'each';
-	
+
 	var defineSingular = this[define + singular] = function(key, value){
 		if (typeOf(key) == 'regexp') matchers.push({'regexp': key, 'value': value, 'type': typeOf(value)});
 		else accessor[key] = value;
 		return this;
 	};
-	
+
 	var definePlural = this[define + plural] = function(object){
 		for (var key in object) accessor[key] = object[key];
 		return this;
 	};
-	
+
 	var lookupSingular = this[lookup + singular] = function(key){
 		if (accessor.hasOwnProperty(key)) return accessor[key];
 		for (var l = matchers.length; l--; l){
 			var matcher = matchers[l], matched = key.match(matcher.regexp);
 			if (matched && (matched = matched.slice(1))){
 				if (matcher.type == 'function') return function(){
-					return matcher.value.apply(this, Array.slice(arguments).append(matched));
+					return matcher.value.apply(this, Array.slice(arguments).concat(matched));
 				}; else return matcher.value;
 			}
 		}
 		return null;
 	};
-	
-	var lookupPlural = this[lookup + plural] = lookupSingular.overloadGetter(true);
-	
+
+	var lookupPlural = this[lookup + plural] = function(){
+		var results = {};
+		for (var i = 0; i < arguments.length; i++){
+			var argument = arguments[i];
+			results[argument] = lookupSingular(argument);
+		}
+		return result;
+	};
+
 	var eachSingular = this[each + singular] = function(fn, bind){
-		Object.each(accessor, fn, bind);
+		forEach(accessor, fn, bind);
+	};
+	
+	var apply = this.apply = function(object){
+		object[define + singular] = defineSingular;
+		object[define + plural] = definePlural;
+		object[lookup + singular] = lookupSingular;
+		object[lookup + plural] = lookupPlural;
+		object[each + singular] = eachSingular;
 	};
 
 };
 
-})();
+});
