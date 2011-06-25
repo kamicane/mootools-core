e448e35467909f366dd79367236dd535a1b37ed5
Valerio Proietti <kamicane@gmail.com>
2011-06-25 19:42:46 +0200

    - the generic forEach now also works with numbers.

--- Source/Utility/forEach.js
+++ Source/Utility/forEach.js
@@ -7,16 +7,16 @@ description: for each
 
 define('Utility/forEach', ['Utility/typeOf', 'Host/Array', 'Host/Object'], function(typeOf, Array, Object){
 	
-	Object.implement('forEach', function(fn, context){
-		for (var key in this) if (this.hasOwnProperty(key)) fn.call(context, this[key], key, this);
-	});
+Object.implement('forEach', function(fn, context){
+	for (var key in this) if (this.hasOwnProperty(key)) fn.call(context, this[key], key, this);
+});
 
-	return function(self, fn, context){
-		switch(typeOf(self)){
-			case 'object': return Object.forEach(self, fn, context);
-			case 'array': return Array.forEach(self, fn, context);
-		}
-		return null;
-	};
+return function(self, fn, context){
+	switch(typeOf(self)){
+		case 'object': Object.forEach(self, fn, context); break;
+		case 'array': Array.forEach(self, fn, context); break;
+		case 'number': for (var i = 0; i < self; i++) fn.call(context, i); break;
+	}
+};
 
 });
