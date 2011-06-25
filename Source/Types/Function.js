6c72b370c0b57201c9aef80cd64a77ee4e9dc57a
Valerio Proietti <kamicane@gmail.com>
2011-06-25 19:34:22 +0200

    - converted Types/Function. Removed pass / delay / periodical.

--- Source/Types/Function.js
+++ Source/Types/Function.js
@@ -2,32 +2,31 @@
 ---
 name: Function
 description: Function prototypes and generics.
-requires: Type
-provides: Function
 ...
 */
 
-Function.extend('attempt', function(){
-	for (var i = 0, l = arguments.length; i < l; i++){
-		try {
-			return arguments[i]();
-		} catch (e){}
+define('Types/Function', ['Host/Function'], function(Function){
+	
+Function.extend({
+
+	from: function(item){
+		return (typeOf(item) == 'function') ? item : function(){
+			return item;
+		};
+	},
+	
+	attempt: function(){
+		for (var i = 0, l = arguments.length; i < l; i++){
+			try {
+				return arguments[i]();
+			} catch (e){}
+		}
+		return null;
 	}
-	return null;
+	
 });
 
 Function.implement({
-	
-	bind: function(bind){
-		var self = this,
-			args = (arguments.length > 1) ? Array.slice(arguments, 1) : null;
-
-		return function(){
-			if (!args && !arguments.length) return self.call(bind);
-			if (args && arguments.length) return self.apply(bind, args.concat(Array.from(arguments)));
-			return self.apply(bind, args || arguments);
-		};
-	},
 
 	attempt: function(args, bind){
 		try {
@@ -35,20 +34,10 @@ Function.implement({
 		} catch (e){}
 
 		return null;
-	},
-
-	pass: function(args, bind){
-		args = (args != null) ? Array.from(args) : [];
-		args.unshift(bind);
-		return this.bind.apply(this, args);
-	},
-
-	delay: function(delay, bind, args){
-		return setTimeout(this.pass((args == null ? [] : args), bind), delay);
-	},
-
-	periodical: function(periodical, bind, args){
-		return setInterval(this.pass((args == null ? [] : args), bind), periodical);
 	}
 
+});
+
+return Function;
+	
 });
