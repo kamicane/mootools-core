41b17b4f7a99ef596711cc02a96b5d2d46b27524
Valerio Proietti <kamicane@gmail.com>
2011-06-26 22:24:02 +0200

    - removed Function.from. updated module paths.

--- Source/Utility/Function.js
+++ Source/Utility/Function.js
@@ -5,38 +5,24 @@ description: Function prototypes and generics.
 ...
 */
 
-define('Types/Function', ['Utility/typeOf', 'Host/Function', 'Host/Array'], function(typeOf, Function, Array){
+define('Utility/Function', ['Utility/typeOf', 'Host/Function', 'Host/Array'], function(typeOf, Function, Array){
 	
-Function.extend({
-
-	from: function(item){
-		return (typeOf(item) == 'function') ? item : function(){
-			return item;
-		};
-	},
-	
-	attempt: function(){
-		for (var i = 0, l = arguments.length; i < l; i++){
-			try {
-				return arguments[i]();
-			} catch (e){}
-		}
-		return null;
+Function.extend('attempt', function(){
+	for (var i = 0, l = arguments.length; i < l; i++){
+		try {
+			return arguments[i]();
+		} catch (e){}
 	}
-	
+	return null;
 });
 
-Function.implement({
+Function.implement('attempt', function(bind){
+	var args = Array.slice(arguments);
+	try {
+		return this.apply(args.shift(0), args);
+	} catch (e){}
 
-	attempt: function(bind){
-		var args = Array.slice(arguments);
-		try {
-			return this.apply(args.shift(0), args);
-		} catch (e){}
-
-		return null;
-	}
-
+	return null;
 });
 
 return Function;
