9c2ea16b1732d2b581e94806257379cc2c002ffe
Valerio Proietti <kamicane@gmail.com>
2011-06-26 21:12:48 +0200

    - re-simplified sandbox instantiation.  - updated host.implement to work with the latest changes.

--- Source/Core/SandBox.js
+++ Source/Core/SandBox.js
@@ -1,3 +1,9 @@
+/*
+---
+name: Sandbox
+description: simple Sandbox
+...
+*/
 
 define('Core/SandBox', [
 	'Utility/typeOf', 'Host/Array', 'Host/Date', 'Host/Function', 'Host/Number', 'Host/Object', 'Host/RegExp', 'Host/String'
@@ -6,24 +12,25 @@ define('Core/SandBox', [
 var hosts = {ARRAY: A, DATE: D, FUNCTION: F, NUMBER: N, OBJECT: O, REGEXP: R, STRING: S},
 	sandboxes = {};
 
-var implement = function(sandbox, key, method){
-	sandbox.prototype[key] = function(){
-		return sb(method.apply(this.self, arguments), this);
-	};
-};
-
 for (var h in hosts) (function(host, h){
 
-	var proto = new host, hostImplement = host.implement, sandbox = sandboxes[h] = function(o, type){
+	var hostImplement = host.implement, sandbox = sandboxes[h] = function(o){
 		this.self = o;
-		this.type = type;
 	};
 
-	for (var p in proto) implement(sandbox, p, proto[p]);
+	sandbox.implement = function(key, method){
+		if (method) this.prototype[key] = function(){
+			return sb(method.apply(this.self, arguments), this);
+		};
+	};
 
-	host.implement = function(key, fn){
-		hostImplement.call(this, key, fn);
-		if (fn && this.prototype[key] == fn) implement(sandbox, key, fn);
+	for (var p in host.prototype) sandbox.implement(p, host.prototype[p]);
+
+	var implement = host.implement = function(key, fn){
+		if (typeof key != 'string') for (var k in key) implement.call(this, k, key[k]); else {
+			hostImplement.call(this, key, fn);
+			if (this.prototype[key]) sandbox.implement(key, fn);
+		}
 		return this;
 	};
 
@@ -37,12 +44,11 @@ for (var h in hosts) (function(host, h){
 
 })(hosts[h], h);
 
-var sb = function(item, sb_){
+var sb = function(item){
 	if (item == null) return null;
 	item = item.valueOf();
-	var type = typeOf(item).toUpperCase(), sandbox;
-	return (sb_ && sb_.type == type) ? (sb_.self = item, sb_)
-		: ((sandbox = sandboxes[type]) ? new sandbox(item, type) : item);
+	var type = typeOf(item).toUpperCase(), sandbox = sandboxes[type];
+	return sandbox ? new sandbox(item) : item;
 };
 
 return sb;
