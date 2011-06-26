43c820ca78cf2afd22c2495f729d6d78da5d09eb
Valerio Proietti <kamicane@gmail.com>
2011-06-26 21:09:49 +0200

    - host.implement and host.extend are not recursively called anymore. Instead, the specific function is used.  - generics are not added to natives any longer when installed.

--- Source/Core/Host.js
+++ Source/Core/Host.js
@@ -19,15 +19,14 @@ return function Host(native_){
 
 	host.install = function(object){
 		object = object || native_;
-		for (key in generics) if (!object[key]) native_[key] = generics[key];
 		for (key in prototypes) if (!object.prototype[key]) object.prototype[key] = prototypes[key];
 		return this;
 	};
 
-	host.implement = function(key, fn){
-		if (typeof key != 'string') for (var k in key) this.implement(k, key[k]); else if (!this.prototype[key] && fn){
+	var implement = host.implement = function(key, fn){
+		if (typeof key != 'string') for (var k in key) implement.call(this, k, key[k]); else if (!this.prototype[key] && fn){
 			var proto = this.prototype[key] = prototypes[key] = (native_.prototype[key] || fn);
-			this.extend(key, function(){
+			extend.call(this, key, function(){
 				var args = slice.call(arguments);
 				return proto.apply(args.shift(0), args);
 			});
@@ -35,8 +34,9 @@ return function Host(native_){
 		return this;
 	};
 
-	host.extend = function(key, fn){
-		if (typeof key != 'string') for (var k in key) this.extend(k, key[k]); else if (!this[key] && fn) generics[key] = this[key] = (native_[key] || fn);
+	var extend = host.extend = function(key, fn){
+		if (typeof key != 'string') for (var k in key) extend.call(this, k, key[k]);
+		else if (!this[key] && fn) generics[key] = this[key] = (native_[key] || fn);
 		return this;
 	};
 
