6272827d3276d514125bd23c99dc8b6d6fb9815e
Valerio Proietti <kamicane@gmail.com>
2011-06-27 19:09:36 +0200

    - since "this" is now a regExp, we need to add toString() before replacing. This will work in the prototype of RegExp when install()ed, and as a generic for both strings and regular expressions.  - forgot to return RegExp

--- Source/Utility/RegExp.js
+++ Source/Utility/RegExp.js
@@ -8,7 +8,9 @@ description: Custom RegExp prototypes and generics.
 define('Utility/RegExp', ['Host/RegExp'], function(RegExp){
 
 RegExp.implement('escape', function(){
-	return this.replace(/([-.*+?^${}()|[\]\/\\])/g, '\\$1');
+	return this.toString().replace(/([-.*+?^${}()|[\]\/\\])/g, '\\$1');
 });
+
+return RegExp;
 
 });
