6bfa3b08f391c25eb748706fae2da9acf9cc80f8
Arian <arian@aryweb.nl>
2011-08-19 01:44:01 +0200

    Add ES5 shims in Host/Array more ES5 compliant, especially Array.map where the last array item is `undefined`

--- Source/Host/Array.js
+++ Source/Host/Array.js
@@ -20,47 +20,47 @@ return Array_.implement({
 
 	filter: function(fn, bind){
 		var results = [];
-		for (var i = 0, l = this.length; i < l; i++){
+		for (var i = 0, l = this.length >>> 0; i < l; i++){
 			if ((i in this) && fn.call(bind, this[i], i, this)) results.push(this[i]);
 		}
 		return results;
 	},
 
 	indexOf: function(item, from){
-		for (var l = this.length, i = (from < 0) ? Math.max(0, l + from) : from || 0; i < l; i++){
+		for (var l = this.length >>> 0, i = (from < 0) ? Math.max(0, l + from) : from || 0; i < l; i++){
 			if (this[i] === item) return i;
 		}
 		return -1;
 	},
 
 	map: function(fn, bind){
-		var results = [];
-		for (var i = 0, l = this.length; i < l; i++){
+		var length = this.length >>> 0, results = Array(length);
+		for (var i = 0, l = length; i < l; i++){
 			if (i in this) results[i] = fn.call(bind, this[i], i, this);
 		}
 		return results;
 	},
 
 	every: function(fn, bind){
-		for (var i = 0, l = this.length; i < l; i++){
+		for (var i = 0, l = this.length >>> 0; i < l; i++){
 			if ((i in this) && !fn.call(bind, this[i], i, this)) return false;
 		}
 		return true;
 	},
 
 	some: function(fn, bind){
-		for (var i = 0, l = this.length; i < l; i++){
+		for (var i = 0, l = this.length >>> 0; i < l; i++){
 			if ((i in this) && fn.call(bind, this[i], i, this)) return true;
 		}
 		return false;
 	},
 
 	forEach: function(fn, context){
-		for (var i = 0, l = this.length; i < l; i++){
+		for (var i = 0, l = this.length >>> 0; i < l; i++){
 			if (i in this) fn.call(context, this[i], i, this);
 		}
 	}
 
 });
-	
+
 });
