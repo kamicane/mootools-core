a34f0f5c18ef87c945be70297967f7cd038b1c1d
Valerio Proietti <kamicane@gmail.com>
2011-09-10 14:37:25 +0200

    - Host can now be subclassed. The result of calling Host() is not a function any longer.

--- Source/Core/Host.js
+++ Source/Core/Host.js
@@ -7,40 +7,50 @@ description: The Host object
 
 define(function(){
 
-var slice = Array.prototype.slice;
+var slice = Array.prototype.slice, create = Object.create || function(self){
+	var F = function(){};
+	F.prototype = self;
+	return new F;
+};
+
+var Host = function(guest){
+	this.guest = guest;
+	this.prototype = {};
+};
+
+var implement = Host.prototype.implement = function(key, fn){
+	if (typeof key != 'string') for (var k in key) implement.call(this, k, key[k]); else if (!this.prototype[key] && fn){
+		var proto = this.prototype[key] = (this.guest.prototype[key] || fn);
+		extend.call(this, key, function(){
+			var args = slice.call(arguments);
+			return proto.apply(args.shift(0), args);
+		});
+	}
+	return this;
+};
+
+var extend = Host.prototype.extend = function(key, fn){
+	if (typeof key != 'string') for (var k in key) extend.call(this, k, key[k]);
+	else if (!this[key] && fn) this[key] = (this.guest[key] || fn);
+	return this;
+};
+
+Host.prototype.install = function(){
+	for (var key in this.prototype) if (!this.guest.prototype[key]) this.guest.prototype[key] = this.prototype[key];
+	return this;
+};
 
 // Host class
 
-return function Host(guest){
+return function(object){
+	
+	if (object instanceof Host){
+		var host = Object.create(object);
+		host.prototype = Object.create(object.prototype);
+		return host;
+	}
 
-	var prototypes = {}, generics = {}, host = function(){
-		return guest.apply(guest, arguments);
-	};
-
-	host.install = function(object){
-		object = object || guest;
-		for (key in prototypes) if (!object.prototype[key]) object.prototype[key] = prototypes[key];
-		return this;
-	};
-
-	var implement = host.implement = function(key, fn){
-		if (typeof key != 'string') for (var k in key) implement.call(this, k, key[k]); else if (!this.prototype[key] && fn){
-			var proto = this.prototype[key] = prototypes[key] = (guest.prototype[key] || fn);
-			extend.call(this, key, function(){
-				var args = slice.call(arguments);
-				return proto.apply(args.shift(0), args);
-			});
-		}
-		return this;
-	};
-
-	var extend = host.extend = function(key, fn){
-		if (typeof key != 'string') for (var k in key) extend.call(this, k, key[k]);
-		else if (!this[key] && fn) generics[key] = this[key] = (guest[key] || fn);
-		return this;
-	};
-
-	return host;
+	return new Host(object);
 
 };
 
